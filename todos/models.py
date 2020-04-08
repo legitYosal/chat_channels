646dd4aa12366eb984add9acfb4fbb094310263e
usefss <flasnm@yahoo.com>
2020-04-08 04:52:08 -0400

    todo model added

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+        todo = models.CharField(
+            max_length = 100,
+            null = False,
+            help_text = 'this field is required'
+        )
+        done = models.BooleanField(default=False)
